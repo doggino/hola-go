e91bc16a093fa58276fd78a1167d007975801140
doggino <dogginopuppet@outlook.com>
2018-04-28 23:22:16 +0200

    Fixes vet and fmt.

--- hola.go
+++ hola.go
@@ -1,51 +1,52 @@
 package hola
 
-import "fmt"
-//import "net/url"
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func Serve() {
-  fmt.Println("running...")
-  http.Handle("/hola", http.HandlerFunc(handleRequest))
-  err := http.ListenAndServe("127.0.0.1:8001", nil)
-  if err != nil {
-    panic(err)
-    return
-  }
-  fmt.Println("finished.")
+	fmt.Println("running...")
+	http.Handle("/hola", http.HandlerFunc(handleRequest))
+	err := http.ListenAndServe("127.0.0.1:8001", nil)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("finished.")
 }
 
 const defaultAnswer = "hola, mundo!"
-func getAnswer(names []string, ok bool) (string) {
 
-  if !ok {
-    return defaultAnswer
-  }
+func getAnswer(names []string, ok bool) string {
 
-  name := names[0]
-  if len(name) < 1 {
-    return defaultAnswer
-  }
+	if !ok {
+		return defaultAnswer
+	}
 
-  return fmt.Sprintf("hola, %v!", names[0])
+	name := names[0]
+	if len(name) < 1 {
+		return defaultAnswer
+	}
+
+	return fmt.Sprintf("hola, %v!", names[0])
 }
 
 func handleGet(w http.ResponseWriter, req *http.Request) {
-  u := req.URL
-  values := u.Query()
-  names, ok := values["name"]
-  fmt.Println(names)
-  fmt.Println(len(names))
+	u := req.URL
+	values := u.Query()
+	names, ok := values["name"]
+	fmt.Println(names)
+	fmt.Println(len(names))
 
-  answer := getAnswer(names, ok)
+	answer := getAnswer(names, ok)
 
-  w.Write([]byte(answer))
+	w.Write([]byte(answer))
 }
 
 func handleRequest(w http.ResponseWriter, req *http.Request) {
-  switch req.Method {
-    case "GET":
-      handleGet(w, req)
-    default:
-  }
+	switch req.Method {
+	case "GET":
+		handleGet(w, req)
+	default:
+	}
 }
--- hola_test.go
+++ hola_test.go
@@ -9,53 +9,52 @@ var mux *http.ServeMux
 
 // This is executed once per test suite
 func TestMain(m *testing.M) {
-  setUp()
-  code := m.Run()
-  os.Exit(code)
+	setUp()
+	code := m.Run()
+	os.Exit(code)
 }
 
 func setUp() {
-  mux = http.NewServeMux()
-  mux.HandleFunc("/hola", handleRequest)
+	mux = http.NewServeMux()
+	mux.HandleFunc("/hola", handleRequest)
 }
 
 func testHandleRequestBoilerplate(url string, expectedAnswer string, t *testing.T) {
-  writer := httptest.NewRecorder()
-  request, _ := http.NewRequest("GET", url, nil)
-  mux.ServeHTTP(writer, request)
+	writer := httptest.NewRecorder()
+	request, _ := http.NewRequest("GET", url, nil)
+	mux.ServeHTTP(writer, request)
 
-  if writer.Code != 200 {
-    t.Errorf("Response code is %v instead of 200.", writer.Code)
-  }
-  answer := writer.Body.String()
-  t.Logf("body: \"%v\"\n", answer)
-  if answer != expectedAnswer {
-    t.Errorf("Body is wrong: \"%v\", expected: \"%v\"\n", answer, expectedAnswer)
-  }
+	if writer.Code != 200 {
+		t.Errorf("Response code is %v instead of 200.", writer.Code)
+	}
+	answer := writer.Body.String()
+	t.Logf("body: \"%v\"\n", answer)
+	if answer != expectedAnswer {
+		t.Errorf("Body is wrong: \"%v\", expected: \"%v\"\n", answer, expectedAnswer)
+	}
 }
 
 func TestHandleRequestNoQuery(t *testing.T) {
-  testHandleRequestBoilerplate("/hola", defaultAnswer, t)
+	testHandleRequestBoilerplate("/hola", defaultAnswer, t)
 }
 
 func TestHandleRequestWeirdQuery(t *testing.T) {
-  testHandleRequestBoilerplate("/hola?number=45", defaultAnswer, t)
+	testHandleRequestBoilerplate("/hola?number=45", defaultAnswer, t)
 }
 
 func TestHandelRequestQueryNameEmpty(t *testing.T) {
-  testHandleRequestBoilerplate("/hola?name=", defaultAnswer, t)
+	testHandleRequestBoilerplate("/hola?name=", defaultAnswer, t)
 }
 
 func TestHandleRequestExpectedQuery(t *testing.T) {
-  testHandleRequestBoilerplate("/hola?name=perico", "hola, perico!", t)
+	testHandleRequestBoilerplate("/hola?name=perico", "hola, perico!", t)
 }
 
 func TestHandleRequestAndFail(t *testing.T) {
-  t.Skip("this test must fail.")
-  testHandleRequestBoilerplate("/hola?name=perico", "hola, manolo!", t)
+	t.Skip("this test must fail.")
+	testHandleRequestBoilerplate("/hola?name=perico", "hola, manolo!", t)
 }
 
 func TestSkip(t *testing.T) {
-  t.Skip("how to skip a test")
+	t.Skip("how to skip a test")
 }
-
